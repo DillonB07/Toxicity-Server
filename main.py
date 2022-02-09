ae173bb53011dbc65cb8659de023cb0b56c6744b
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2022-02-09 19:27:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect
+from os import environ
+from googleapiclient import discovery
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/<message>')
+def api_message(message: str = ''):
+    API_KEY = environ['PERSPECTIVE_KEY']
+
+    client = discovery.build(
+        "commentanalyzer",
+        "v1alpha1",
+        developerKey=API_KEY,
+        discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+        static_discovery=False,
+    )
+
+    analyze_request = {
+        'comment': { 'text': message },
+        'requestedAttributes': {'TOXICITY': {}}
+    }
+    
+    response = client.comments().analyze(body=analyze_request).execute()
+    print(json.dumps(response, indent=4))
+    
+    return response.json()
+
+@app.route('/')
+@app.route('/<text>')
+def home(text: str = None):
+  return redirect('https://dillonb07.github.io/toxicity-checker')
+
+app.run(host='0.0.0.0', port=8080)
